f4a49cc81545d2c5ecc329ec6428e6a09e2bd364
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2025-11-02 16:14:07 -0800

    2025 page

---
+++ site/public/index.js
@@ -0,0 +1,79 @@
+document.addEventListener("mousemove", event => {
+    const x = (event.clientX / document.body.clientWidth - 0.5) * 2, y = (event.clientY / document.body.clientHeight - 0.5) * 2;
+    const logo = document.getElementById("content");
+
+    logo.style.transform = `translateX(${x * 1}%) translateY(${y * 1}%)`;
+});
+
+function updateTimer(timer, remaining) {
+    const parts = [
+        Math.floor((remaining % 86400) / 3600),
+        Math.floor(((remaining % 86400) % 3600) / 60),
+        Math.floor(((remaining % 86400) % 3600) % 60),
+        Math.floor(remaining / 86400),
+    ];
+
+    timer.innerHTML = (parts[3] > 0 ? `${parts[3]}.` : "") + parts.slice(0, 3).map(v => v.toString().padStart(2, "0")).join(":");
+}
+
+const CDB_URL = "https://content.luanti.org";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    // Dates
+    const dates = [
+        ["til theme announcement + jam start", new Date("2025-11-08T00:00Z")],
+        ["to work on your games", new Date("2025-11-29T00:00Z")],
+        ["to finish and get your game approved", new Date("2025-11-30T00:00Z")],
+        [`to <a href="/rating">rank submissions</a>`, new Date("2025-12-08T00:00Z")],
+        ["Results soon"],
+    ];
+
+    [...document.getElementsByClassName("date")].forEach(e => {
+        const when = e.getAttribute("data-date");
+        e.innerHTML = !Date.parse(when) ? "DATE PARSE ERROR" : new Date(when).toLocaleString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            hour12: true,
+            minute: "2-digit",
+            timeZoneName: "short"
+        });
+    });
+
+    // Closest date
+    let i = 0;
+    let next = dates[0];
+    while (next[1] && next[1] <= Date.now()) {
+        next = dates[++i];
+    }
+
+    let el_next = document.getElementById("next-event");
+    el_next.innerHTML = next[0];
+
+    // Countdown
+    const timer = document.getElementById("clock");
+    setInterval(() => {
+        if (Date.parse(next[1])) {
+            const diff = Math.max(0, (next[1] - Date.now()) / 1000);
+            updateTimer(timer, diff);
+
+            // Update closest date
+            if (diff < 1) {
+                next = dates[++i];
+                el_next.innerHTML = next[0];
+            }
+        } else {
+            timer.innerHTML = next[0];
+            el_next.innerHTML = "";
+        }
+    }, 1000);
+
+    if (Date.parse(next[1])) {
+        updateTimer(timer, Math.max(0, (next[1] - Date.now()) / 1000));
+    } else {
+        timer.innerHTML = next[0];
+        el_next.innerHTML = "";
+    }
+});
