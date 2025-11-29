5e11e6d1a01ce9316f8ee08bd36156cb345390dc
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2025-11-28 17:36:44 -0800

    HTTPS

--- site/public/rating/config.js
+++ site/public/rating/config.js
@@ -1,6 +1,6 @@
 export const config = {
     "oauth_client_id": "ec984104ccdddfcee223e2b4",
-    "server_address": "http://backend.greenxenith.com",
+    "server_address": "https://backend.greenxenith.com",
     "jam_tag": "jam_game_2024",
     "queue_window": 5, // Time between updates
     "disabled": false,
